16c4ede1922c0a1504ccbb3259b531d7602795e8
MovemDimon <movem9013@gmail.com>
2025-08-02 08:44:17 +0330

    Update db.py

--- app/db.py
+++ app/db.py
@@ -3,5 +3,5 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.config import settings
 
 client = AsyncIOMotorClient(settings.DATABASE_URL)
-db = client.get_default_database()
+db = client["mydatabase"] 
 transactions = db["transactions"]
